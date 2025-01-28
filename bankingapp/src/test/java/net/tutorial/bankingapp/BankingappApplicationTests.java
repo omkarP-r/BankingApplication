aa1edb7bedbb67e51fbea169221758c494ce22d6
OMKAR <omkar29pawar@gmail.com>
2025-01-28 19:07:29 +0530

    first commit

---
+++ bankingapp/src/test/java/net/tutorial/bankingapp/BankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tutorial.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
