2a315cfeed0e1801325df585c7d508a4f2e38e36
OMKAR <omkar29pawar@gmail.com>
2025-01-30 20:04:08 +0530

    exception package created

---
+++ bankingapp/src/main/java/net/tutorial/bnkingapp/exception/BankingappNotFoundException.java
@@ -0,0 +1,17 @@
+package net.tutorial.bnkingapp.exception;
+
+public class BankingappNotFoundException  extends RuntimeException{
+
+	public BankingappNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BankingappNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
