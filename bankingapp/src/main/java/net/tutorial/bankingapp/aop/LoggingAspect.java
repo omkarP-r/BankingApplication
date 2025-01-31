2d8892cb4ba9da27a6487fa3d8e95d58e5627805
OMKAR <omkar29pawar@gmail.com>
2025-01-31 17:20:40 +0530

    executed aop

--- bankingapp/src/main/java/net/tutorial/bankingapp/aop/LoggingAspect.java
+++ bankingapp/src/main/java/net/tutorial/bankingapp/aop/LoggingAspect.java
@@ -1,5 +1,16 @@
 package net.tutorial.bankingapp.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+
 public class LoggingAspect {
 
+	@Before("execution(* net.tutorial.bankingapp.service.AccountServiceImpl.getAccountById(..))")
+	public void log() {
+		System.out.println("Aspect Log called");
+	}
 }
